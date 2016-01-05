52764af587faa74046e10f12550afa7c67dbbbb0
Thomas Bachmann <thomas.bachmann@mambu.com>
2016-01-05 12:00:15 +0100

    Added simple integration test case for uploading to S3, create application version and deploy application version

---
+++ test/com/mambu/ant/ElasticBeanstalkUploadTest.java
@@ -0,0 +1,30 @@
+package com.mambu.ant;
+
+/**
+ * Simple locally executable system test
+ */
+public class ElasticBeanstalkUploadTest {
+
+	public static void main(String[] args) {
+
+		ElasticBeanstalkUpload elasticBeanstalkUpload = new ElasticBeanstalkUpload();
+
+		elasticBeanstalkUpload.setApplicationName("");
+		elasticBeanstalkUpload.setAwsAccessKey("");
+		elasticBeanstalkUpload.setAwsSecretKey("");
+		elasticBeanstalkUpload.setBeanstalkEndpoint("elasticbeanstalk.us-east-1.amazonaws.com");
+		elasticBeanstalkUpload.setCreateApplicationVersion("true");
+		elasticBeanstalkUpload.setDeployApplicationVersion("true");
+		elasticBeanstalkUpload.setEndpoint("s3.amazonaws.com");
+		elasticBeanstalkUpload.setEnvironmentName("");
+		elasticBeanstalkUpload.setS3Bucket("");
+		elasticBeanstalkUpload.setS3Key("");
+		elasticBeanstalkUpload.setUploadToS3("true");
+		elasticBeanstalkUpload.setVersionLabel("");
+		elasticBeanstalkUpload.setWarFileLocation("");
+
+		elasticBeanstalkUpload.execute();
+
+	}
+
+}
